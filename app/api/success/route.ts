155a9af5c0a0fa9a405d4ff0014a11432ac8e972
ledemkam <81742990+ledemkam@users.noreply.github.com>
2024-06-02 19:29:56 +0200

    Update route.ts

--- app/api/success/route.ts
+++ app/api/success/route.ts
@@ -2,33 +2,37 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET as string, {
-  apiVersion: "2024-04-10",
+  apiVersion: '2024-04-10', 
 });
+
 
 export const GET = async (request: NextRequest) => {
   try {
+
     const { searchParams } = new URL(request.url);
-    const token = searchParams.get("token");
+    const token = searchParams.get('token'); 
+
 
     if (!token) {
-      return NextResponse.json({ error: "Token is required" }, { status: 400 });
+      return NextResponse.json({ error: 'Token is required' }, { status: 400 });
     }
 
+ 
     const customer = await stripe.customers.retrieve(token);
 
+
     if ((customer as Stripe.DeletedCustomer).deleted) {
-      return NextResponse.json(
-        { error: "Customer not found" },
-        { status: 404 }
-      );
+      return NextResponse.json({ error: 'Customer not found' }, { status: 404 });
     }
 
-    const customerData = customer as Stripe.Customer;
-    return NextResponse.json(customerData, { status: 200 });
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Error retrieving customer data" },
-      { status: 500 }
-    );
+    const customerData = customer as Stripe.Customer;  
+    return NextResponse.json({
+      name: customerData.name, 
+      email: customerData.email, 
+    }, { status: 200 });
+
+  } catch (error: any) {
+    console.error("Error occurred:", error);
+    return NextResponse.json({ error: 'Error retrieving customer data' }, { status: 500 });
   }
 };
